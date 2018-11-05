85a391aeb3ddbe8221ba3492d0a9a3925bd4d18f
Goorara <tey@nate.com>
2018-11-06 01:38:34 +0900

    insert check opcode check.c

--- check.c
+++ check.c
@@ -5,11 +5,10 @@ int is_valid(char *op, char *args)
 	//printf("if valid, return 1\n");
 	//printf("otherwise, return 0\n");
 
-	/********************************/
-    /*  							*/
-	/*         syntax check         */
-    /*  							*/
-	/********************************/
+        if(strcmp(op,"mov")==1)
+        {
+            return 0;
+        }
 	
 	return 1;
 }
