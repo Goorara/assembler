dc06b05b9f0b113fc4ff995389129def262d1b0c
Goorara <tey@nate.com>
2018-11-14 04:53:28 +0900

    complete check.c change condition

--- check.c
+++ check.c
@@ -1,77 +1,593 @@
 #include "myas.h"
 
+ 
+
 int is_valid(char *op, char *args)
+
 {
-	char regi[] = "%eax %ecx %edx %ebx %ebp %edi %esp";
-	char copy[256] = {NULL, };
 
-	strcpy(copy,args);
+	char regi[] = "%eax %ecx %edx %ebx %esp %ebp %esi %edi";
 
-	char* ptr = strtok(copy, ",");
-	char* value[40] = {NULL, }; 
-	char ch[20] = {NULL, };
-        
+	char hx_digit[] = "0 1 2 3 4 5 6 7 8 9 a b c d e f A B C D E F";
+
+	char scale[] = "1 2 4 8";
+
+ 
+
+	char copy[100] = { NULL , };
+
+ 
+
+	strcpy(copy, args);
+
+ 
+
+	char ch1[100] = { NULL , };
+
+	char ch2[100] = { NULL , };
+
+ 
+
+	char *re_save[100] = { NULL , };
+
+	char *re1;
+
+	char *re2;
+
+	char *rere1 = malloc(sizeof(char) * 30);
+
+ 
+
+	int len = strlen(args);
+
+ 
+
+	int c = 0;
+
 	int i = 0;
-        int j = 0;
-        int k = 0;
-        int flag = 0;
-        
-        //printf("if valid, return 1\n");
-	//printf("otherwise, return 0\n");
 
-        while(ptr != NULL)
-        {
-           value[i] = ptr;
-           i++;
+	int k = 0;
 
-           ptr - strtok(NULL, " ");
-        }
+	int l = 0;
 
-        for(j = 0; j < 40; j++)
-        {
-           if(value[j] != NULL)
-           {
-              ch[j] = *value[j];
-           }
-        }
+	int j = 0;
 
+	
 
-        if(strcmp(op, "mov") == 1)
-        {
-           flag++;
-        }
+	int com_count = 0;
 
-        while(k < i)
-        {
-          switch(ch[k])
-          {
-          case '%':
-             if(strstr(regi, value[k]) == NULL)
-             {
-                flag++;
-             }
-             break;
-          case '+':
-             break;
-          case '-':
-             break;
-          case '(':
-             break;
-          case '0':
-             break;
-          case '$':
-             break;
-          default:
-             flag++;
-             break;
-          }
-          k++;
-        }
+	int lp_count = 0;
 
-        if(flag != 0)
-        {
-           return 0;
-        }
-        	
+	int flag = 0;
+
+	int value_count = 0;
+
+ 
+
+	int lastcmm = 0;
+
+ 
+
+	for (c = len- 1; c >= 0; c--)
+
+	{
+
+		if (copy[c] == ',') 
+
+		{
+
+			lastcmm = c;
+
+			break;
+
+		}
+
+	}
+
+ 
+
+ 
+
+	for (k = 0; k < lastcmm; k++)
+
+	{
+
+		if (copy[k] == ',')
+
+		{
+
+			com_count++;
+
+			ch1[k] = ' ';
+
+ 
+
+		}
+
+		else
+
+		ch1[k] = copy[j];
+
+ 
+
+		j++;
+
+	}
+
+ 
+
+	for (i = 0; i < lastcmm; i++)
+
+	{
+
+		if (copy[i] == '(')
+
+		{
+
+			lp_count++;
+
+			ch1[i] = ' ';
+
+		}
+
+	}
+
+ 
+
+	if (copy[lastcmm - 1] == ')')
+
+		{
+
+			lp_count++;
+
+			ch1[lastcmm - 1] = ' ';
+
+		}
+
+ 
+
+ 
+
+	for (i = lastcmm+1; i <= len; i++)
+
+	{
+
+		ch2[l] = copy[i];
+
+		l++;
+
+	}
+
+ 
+
+	re1 = ch1;
+
+	re2 = ch2;
+
+ 
+
+	c = 0;
+
+ 
+
+	if (com_count != 0 || lp_count == 2 )
+
+	{
+
+		rere1 = strtok(ch1, " ");
+
+ 
+
+		while (rere1 != NULL)
+
+		{
+
+			re_save[c] = rere1;
+
+			rere1 = strtok(NULL, " ");
+
+			
+
+			c++;
+
+		}
+
+	}
+
+	else if (com_count > 2)
+
+	{
+
+		return 0;
+
+	}
+
+ 
+
+	value_count = c;
+
+ 
+
+	i = 0;
+
+	k = 0;
+
+	l = 0;
+
+ 
+
+	if (strcmp(op, "mov") == 1)
+
+	{
+
+		return 0;
+
+	}
+
+ 
+
+	while (1)
+
+	{
+
+		if (lp_count == NULL)
+
+		{
+
+			switch (ch1[k])
+
+			{
+
+			case '%':
+
+				if (strstr(regi, re1) != NULL) printf("This is register\n");
+
+				else
+
+				{
+
+					return 0;
+
+				}
+
+				break;
+
+			case '0':
+
+				if (ch1[1] == 'x')
+
+				{
+
+					for (i = 2; i < lastcmm; i++)
+
+					{
+
+						if (strchr(hx_digit, ch1[i]) != NULL);
+
+						else
+
+						{
+
+							return 0;
+
+						}
+
+					}
+
+				}
+
+				printf("This is memory\n");
+
+				break;
+
+			case '$':
+
+				if (ch1[1] == '0' && ch1[2] == 'x')
+
+				{
+
+					for (i = 3; i < lastcmm; i++)
+
+					{
+
+						if (strchr(hx_digit, ch1[i]) != NULL);
+
+						else
+
+						{
+
+							return 0;
+
+						}
+
+					}
+
+				}
+
+				printf("This is immediate \n");
+
+				break;
+
+			default:
+
+				printf("not value\n");
+
+				return 0;
+
+				break;
+
+			}
+
+		}
+
+		else if (lp_count == 2)
+
+		{
+
+ 
+
+			switch (com_count)
+
+			{
+
+			case 0:
+
+				switch (ch1[k])
+
+				{
+
+				case '-':
+
+					if (ch1[1] == '0' && ch1[2] == 'x')
+
+					{
+
+						for (i = 3; i < strlen(re_save[0]); i++)
+
+						{
+
+							if (strchr(hx_digit, ch1[i]) != NULL);
+
+							else
+
+							{
+
+								return 0;
+
+							}
+
+						}
+
+					}
+
+					printf("This is Memmory \n");
+
+					break;
+
+				default:
+
+					printf("not value\n");
+
+					return 0;
+
+					break;
+
+				}
+
+				break;
+
+			case 1:
+
+				switch (ch1[k])
+
+				{
+
+				case ' ':
+
+					if (strstr(regi, re_save[0]) != NULL &&strstr(regi, re_save[1]) != NULL) printf("This is Memmory \n");
+
+					else
+
+					{
+
+						return 0;
+
+					}
+
+					break;
+
+				default:
+
+					printf("not value\n");
+
+					return 0;
+
+					break;
+
+				}
+
+				break;
+
+			case 2:
+
+				switch (ch1[k])
+
+					{
+
+				case ' ':
+
+					if (strstr(regi, re_save[0]) != NULL && strstr(regi, re_save[1]) != NULL && strstr(scale, re_save[2]) != NULL) printf("This is Memmory \n");
+
+					else
+
+					{
+
+						return 0;
+
+					}
+
+					break;
+
+				case '0':
+
+					if (ch1[1] == 'x')
+
+					{
+
+						for (i = 2; i < strlen(re_save[0]); i++)
+
+						{
+
+							if (strchr(hx_digit, ch1[i]) != NULL);
+
+							else
+
+							{
+
+								return 0;
+
+							}
+
+						}
+
+						if (strstr(regi, re_save[1]) != NULL && strstr(regi, re_save[2]) != NULL && strstr(scale, re_save[3]) != NULL) printf("This is Memmory \n");
+
+						else return 0;
+
+					}
+
+				case '-':
+
+					if (ch1[1] == '0' && ch1[2] == 'x')
+
+					{
+
+						for (i = 3; i < strlen(re_save[0]); i++)
+
+						{
+
+							if (strchr(hx_digit, ch1[i]) != NULL);
+
+							else
+
+							{
+
+								return 0;
+
+							}
+
+						}
+
+						if (strstr(regi, re_save[1]) != NULL && strstr(regi, re_save[2]) != NULL && strstr(scale, re_save[3]) != NULL) printf("This is Memmory \n");
+
+						else return 0;
+
+					}
+
+					break;
+
+					default:
+
+						printf("not value\n");
+
+						return 0;
+
+						break;
+
+					}
+
+				break;
+
+				default:
+
+					printf("not value\n");
+
+					return 0;
+
+				break;
+
+			}
+
+		}
+
+		else
+
+			return 0;
+
+		break;
+
+	}
+
+ 
+
+	k = 0;
+
+ 
+
+	while (1)
+
+	{
+
+		switch (ch2[k])
+
+		{
+
+		case '%':
+
+			if (strstr(regi, re2) != NULL) printf("This is register\n");
+
+			else
+
+			{
+
+				return 0;
+
+			}
+
+			break;
+
+		case '0':
+
+			for (i = 2; i < strlen(ch2); i++)
+
+			{
+
+				if (strchr(hx_digit, ch2[i]) != NULL);
+
+				else
+
+				{
+
+					return 0;
+
+				}
+
+			}
+
+			printf("This is memory\n");
+
+			break;
+
+		default:
+
+			printf("not value\n");
+
+			return 0;
+
+			break;
+
+		}
+
+		break;
+
+	}
+
+	
+
+	free(rere1);
+
 	return 1;
+
+ 
+
 }
